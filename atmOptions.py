529fcf4c2830904e5f2bcd7337d0fe9367d1773f
digipa <megyinh@gmail.com>
2021-04-05 21:46:53 -0400

    Abstracted functionality into bank operation functions named balance, withdrawal, deposit, complaint, and invalidOption.

--- atmOptions.py
+++ atmOptions.py
@@ -1,65 +1,121 @@
-def option():
-    print('\n What would you like to do?: 1: Balance 2: Withdrawal 3: Cash Deposit 4: Complaint 5: Exit \n')
+def mainOptions():
 
-    def selection():
-        return int(input("Please select an option: \n"))
+    selection = int(input('What would you like to do?: 1: Balance 2: Withdrawal 3: Cash Deposit 4: Complaint 5: Main Options 6: Exit \n'))
 
-    choice = selection()
+    if selection == 1:
+        balance()
 
-    if choice == 1:
-        print('1: Balance 2: Main Screen')
+    elif selection == 2:
+        withdrawal()
 
-        choice2 = selection()
-        if choice2 == 1:
-            print('Your balance is %s.' % sum(accountBalance))
-            option()
-        elif choice2 == 2:
-            option()
-        else:
-            print('Invalid Option selected, please try again')
-            
-    if choice == 2:
-        print('1: Withdrawal 2: Main Screen')
+    elif selection == 3:
+        deposit()
 
-        choice2 = selection()
-        if choice2 == 1:
-            accountBalance.append(-int(input('How much would you like to withdraw? \n')))
-            print('Take your cash \n')
-            option()
-        elif choice2 == 2:
-            option()
-        else:
-            print('Invalid Option selected, please try again')
+    elif selection == 4:
+        complaint()
 
-    elif choice == 3:
-        print('1. Cash Deposit 2: Main Screen')
+    elif selection == 5:
+        mainOptions()
 
-        choice2 = selection()
-        if choice2 == 1:
-            accountBalance.append(int(input('How much would you like to deposit? \n')))
-            print('Your current balance is %s' % sum(accountBalance))
-            option()
-        elif choice2 == 2:
-            option()
-        else:
-            print('Invalid Option selected, please try again')
-
-    elif choice == 4:
-        print('1. Complaint 2: Main Screen')
-
-        choice2 = selection()
-        if choice2 == 1:
-            input('What issue will you like to report? \n')
-            print('Thank you for contacting us')
-            option()
-        elif choice2 == 2:
-            option()
-        else:
-            print('Invalid Option selected, please try again')
-
-    elif choice == 5:
+    elif selection == 6:
         print('Thank you %s. We look forward to seeing you again.' % name)
 
     else:
-        print('Invalid Option selected, please try again')
-        option()
+        invalidOption()
+    mainOptions()
+
+
+
+def balance():
+    print('Your balance is %s.' % sum(accountBalance))
+    
+def withdrawal():
+    accountBalance.append(-int(input('How much would you like to withdraw? \n')))
+    print('Take your cash \n')
+
+def deposit():
+    accountBalance.append(int(input('How much would you like to deposit? \n')))
+    print('Your current balance is %s' % sum(accountBalance))
+
+def complaint():
+    input('What issue will you like to report? \n')
+    print('Thank you for contacting us')
+
+def invalidOption():
+    print('That is an invalid option')
+
+
+
+
+
+# def mainOptions():
+
+#     selection =return int(input('What would you like to do?: 1: Balance 2: Withdrawal 3: Cash Deposit 4: Complaint 5: Exit \n')
+
+#     if selection == 1:
+#         print('1: Balance 2: Main Screen')
+
+#         if selection == 1:
+#             balance()
+
+#         elif selection == 2:
+#             mainOptions()
+#         else:
+#             invalidOption()
+#         mainOptions()
+
+#     if selection == 2:
+#         print('1: Withdrawal 2: Main Screen')
+
+#         if selection == 1:
+#             balance()
+#         elif selection == 2:
+#             mainOption()
+#         else:
+#             invalidOption()
+
+#     elif selection == 3:
+#         print('1. Cash Deposit 2: Main Screen')
+
+#         if selection == 1:
+#         elif selection == 2:
+#             mainOption()
+#         else:
+#             invalidOption()
+
+#     elif selection == 4:
+#         print('1. Complaint 2: Main Screen')
+
+#         if selection == 1:
+#             mainOption()
+#         elif selection == 2:
+#             mainOption()
+#         else:
+#             invalidOption()
+
+#     elif selection == 5:
+#         print('Thank you %s. We look forward to seeing you again.' % name)
+
+#     else:
+#         invalidOption()
+
+
+
+# def balance():
+#     print('Your balance is %s.' % sum(accountBalance))
+#     option()
+    
+# def withdrawal():
+#     accountBalance.append(-int(input('How much would you like to withdraw? \n')))
+#     print('Take your cash \n')
+
+# def deposit():
+#     accountBalance.append(int(input('How much would you like to deposit? \n')))
+#     print('Your current balance is %s' % sum(accountBalance))
+
+# def complaint():
+#     input('What issue will you like to report? \n')
+#     print('Thank you for contacting us')
+
+# def invalidOption():
+#     print('That is an invalid option')
