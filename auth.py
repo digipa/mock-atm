092ca12a7c291ed901ae7f4e7ca20f758f9157a0
digipa <megyinh@gmail.com>
2021-04-06 18:17:16 -0400

    Replaced input with raw_input and modified the conditional statement to append the user input to the allowedPasswords list if the passwords match in the file named auth.py. Cleaned up comments in file named main.py.

--- auth.py
+++ auth.py
@@ -1,8 +1,8 @@
-# --- Welcome ---
-import random
-import datetime
+# # # --- Welcome ---
+# import random
+# import datetime
 
-x = datetime.datetime.now()
+# x = datetime.datetime.now()
 
 database = {}
 
@@ -12,15 +12,15 @@ firstName = []
 lastName = []
 email = []
 
-# --- Login ---
-# account number, password
-# --- Generate Account Number ---
+# # --- Login ---
+# # account number, password
+# # --- Generate Account Number ---
 
 def login():
     print('Login')
-    name = input("Username: \n")
+    username = raw_input("Username: \n")
     if name in allowedUsers:
-        password = input("Password: \n")
+        password = raw_input("Password: \n")
         userId = allowedUsers.index(name)
         if password == allowedPasswords[userId]:
             print('Welcome %s' % name)
@@ -28,42 +28,44 @@ def login():
             print('Password Incorrect, please try again')
     else:
         print('Name not found, please try again')
+login()
 
 def register():
-    firstName.append(input("First Name \n"))
-    lastName.append(input("Last Name \n"))
-    allowedUsers.append(input("Username \n"))
-    email.append(input("Email \n"))
-    createdPassword = allowedPasswords.append(input("Password \n"))
-# set allowedPasswords to match the second password input
-    checkPassword = allowedPasswords.append(input("Password \n"))
-    if checkPassword == createdPassword:
-        print('Passwords match')
+    firstName.append(raw_input("First Name \n"))
+    lastName.append(raw_input("Last Name \n"))
+    allowedUsers.append(raw_input("Username \n"))
+    email.append(raw_input("Email \n"))
+    password = raw_input("Password \n")
+# set allowedPasswords to match the second password raw_input
+    checkPassword = raw_input("Password \n")
+    if checkPassword == password:
+        allowedPasswords.append(checkPassword)
     else:
         print('Passwords do not match')
     login()
+register()
 
-def welcome():
-    print("Welcome to GCU Bank. %s" % x.strftime("%c"))
-    haveAccount = int(input("Do you have an account with us? 1: yes 2: no \n"))
-    if haveAccount == 1:
-        login()
-    elif haveAccount == 2:
-        register()
-    else:
-        print("Option invalid")
-        welcome()
-welcome()
-
-
-
-accountNumber = generateAccountNumber()
-
-database[accountNumber] = [ firstName, lastName, email, password ]
-
-# --- Register ---
-# first name, last name, password, password match, email
+# def welcome():
+#     print("Welcome to GCU Bank. %s" % x.strftime("%c"))
+#     haveAccount = int(raw_input("Do you have an account with us? 1: yes 2: no \n"))
+#     if haveAccount == 1:
+#         login()
+#     elif haveAccount == 2:
+#         register()
+#     else:
+#         print("Option invalid")
+#         welcome()
+# welcome()
 
 
-def generateAccountNumber():
-    return random.randrange(1000000000, 9999999999)
+
+# accountNumber = generateAccountNumber()
+
+# database[accountNumber] = [ firstName, lastName, email, password ]
+
+# # --- Register ---
+# # first name, last name, password, password match, email
+
+
+# def generateAccountNumber():
+#     return random.randrange(1000000000, 9999999999)
--- main.py
+++ main.py
@@ -5,15 +5,3 @@ auth.welcome
 auth.login
 auth.register
 atmOptions.mainOptions
-
-# name = input("What is your Username? \n")
-# if name in allowedUsers:
-#     password = input("What is your Password \n")
-#     userId = allowedUsers.index(name)
-#     if password == allowedPasswords[userId]:
-#         print('Welcome %s' % name)
-#         option()
-#     else:
-#         print('Password Incorrect, please try again')
-# else:
-#     print('Name not found, please try again')
