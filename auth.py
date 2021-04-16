a4689df79d9da8b1da4397bece6d124ac150adcf
digipa <{ID}+{username}@users.noreply.github.com>
2021-04-16 18:51:52 -0400

    Replaced variables named with camelCase with variables named using snake_case in file named auth.py, replaced raw_input() with input() to reflect updated version of Python, updated .gitignore to include xml and iml files.

--- auth.py
+++ auth.py
@@ -9,15 +9,13 @@ x = datetime.datetime.now()
 
 database = {}
 
-# def generateAccountNumber():
-#     return random.randrange(1000000000, 9999999999)
 
 def welcome():
     print("Welcome to GCU Bank. %s" % x.strftime("%c"))
-    haveAccount = int(input("Do you have an account with us? 1: yes 2: no \n"))
-    if haveAccount == 1:
+    have_account = int(input("Do you have an account with us? 1: yes 2: no \n"))
+    if have_account == 1:
         login()
-    elif haveAccount == 2:
+    elif have_account == 2:
         register()
     else:
         print("Option invalid")
@@ -29,12 +27,12 @@ def welcome():
 
 def login():
     print('LOGIN')
-    accountNumberCheck = int(input("What is your account number? \n"))
-    passwordCheck = raw_input("What is your password \n")
+    account_number_check = int(input("What is your account number? \n"))
+    password_check = input("What is your password \n")
 
-    for accountNumber, userDetails in database.items():
-        if accountNumber == accountNumberCheck:
-            if userDetails[3] == passwordCheck:
+    for account_number, userDetails in database.items():
+        if account_number == account_number_check:
+            if userDetails[3] == password_check:
                 mainOptions(userDetails)
                 
     print('Invalid account or password')
@@ -42,23 +40,21 @@ def login():
 
 
 # # --- Register ---
-# # first name, last name, password, password match, email
+# # first name, last name, password, password match, username, email
 
 def register():
     print('REGISTER')
-    firstName = raw_input("First Name \n")
-    lastName = raw_input("Last Name \n")
-    # user = input("Username \n")
-    email = raw_input("Email \n")
-    password = raw_input("Password \n")
-    passwordCheck = raw_input("Re-enter password \n")
-    accountNumber = generateAccountNumber()
-    database[accountNumber] = [ firstName, lastName, email, password]
-    print('Your account number is: %d \n' % accountNumber)
+    first_name = input("First Name \n")
+    last_name = input("Last Name \n")
+    email = input("Email \n")
+    password = input("Password \n")
+    account_number = generate_account_number()
+    database[account_number] = [ first_name, last_name, email, password]
+    print('Your account number is: %d \n' % account_number)
 
     login()
 
-def generateAccountNumber():
+def generate_account_number():
     return random.randrange(1000000000, 9999999999)
 
 welcome()
